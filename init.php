9094364e91cc926064e2a44719dbfac5bc34d6b7
Pierre Blarre <pierre@negative-network.com>
2014-03-20 13:34:30 +0100

    define _MPDF_PATH as absolute PATH, to avoid error : "Not a TrueType font: version=0"

--- init.php
+++ init.php
@@ -1,4 +1,6 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
+if (!defined('_MPDF_PATH')) define('_MPDF_PATH', dirname(MODPATH.basename(__DIR__).DIRECTORY_SEPARATOR.'mpdf').DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'mpdf'.DIRECTORY_SEPARATOR);
+
 // Loads mPDF
 require_once Kohana::find_file('vendor', 'mpdf/mpdf');
